fcc3c9578fcec1933d7a4e2e7f7826b35ce2ebd2
kertak90 <35306203+kertak90@users.noreply.github.com>
2019-07-02 23:34:48 +0300

    не работает кнопка 404

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("12345");
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            //return Content("12345");
+            return View();
+        }
+
+    }
+}
