556486a8ca5589a7158e14afdff800e281dc210f
kertak90 <35306203+kertak90@users.noreply.github.com>
2019-07-14 17:30:54 +0300

    Hometask ended

---
+++ WebStore/Infrastructure/Implementations/SqlProductService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Filter;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class SqlProductService : IProductService
+    {
+        private readonly WebStoreContext _context;
+
+        public SqlProductService(WebStoreContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _context.Brands.ToList();
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter)
+        {
+            var products = _context.Products.AsQueryable();     //В данной фильтрации, фильтрация будет происходить на уровне базы данных
+            if (filter.SectionId.HasValue)
+                products = products.Where(p => p.SectionId == filter.SectionId);
+            if (filter.BrandId.HasValue)
+                products = products.Where(p => p.BrandId == filter.BrandId);
+
+            return products.ToList();
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return _context.Sections.ToList();
+        }
+    }
+}
